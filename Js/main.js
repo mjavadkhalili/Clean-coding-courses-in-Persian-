d84d97b9a76cbbdef09ff31f936212a10e48383c
محمد جواد خلیلی <mamadjavadkhalili@gmail.com>
2021-02-12 20:16:22 +0330

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,17 @@
+/****
+ * Bad
+ */
+
+function fn_hello(name,family)
+{
+    return 'Hello '+ name+' '+family;
+}
+
+
+/****
+ * Good
+ */
+function sayhello(name,family)
+{
+    return 'Hello '+ name+' '+family;
+}
